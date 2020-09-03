38b1b9b06f39fe1f713a9141da84dc02a23d05b0
freitasmco <freitasmco@gmail.com>
2020-09-02 23:29:39 -0300

    Alteração

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-import express from "express";
-import banksRouter from "./routes/banksRouter.js";
-import mongoose from "mongoose";
+// teste
+import express from 'express';
+import banksRouter from './routes/banksRouter.js';
+import mongoose from 'mongoose';
 
 (async () => {
-  try {
-    console.log("Conectando ao MongoDB... ");
-    await mongoose.connect(
-      "mongodb+srv://igti:fullstack@cluster0.pv9x2.mongodb.net/igti_modulo_4?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      }
-    );
-    console.log("Conectado com sucesso ao MongoDB");
-  } catch (error) {
-    console.log("Erro ao conectar no MongoDB. " + error);
-  }
+    try {
+        console.log('Conectando ao MongoDB... ');
+        await mongoose.connect(
+            'mongodb+srv://igti:fullstack@cluster0.pv9x2.mongodb.net/igti_modulo_4?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false
+            }
+        );
+        console.log('Conectado com sucesso ao MongoDB');
+    } catch (error) {
+        console.log('Erro ao conectar no MongoDB. ' + error);
+    }
 })();
 
 const app = express();
@@ -25,5 +26,5 @@ app.use(express.json());
 app.use(banksRouter);
 
 app.listen(3000, () => {
-  console.log("API Bank Started...");
+    console.log('API Bank Started...');
 });
